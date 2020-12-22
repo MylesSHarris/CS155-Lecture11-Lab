ce06c98fdadcfa81f076a401eca9ae41dd7df0e4
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-12-21 21:58:18 -0800

    Add files via upload

---
+++ Lab.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab
+{
+    public class Lab
+    {
+        static Dictionary<String, string> dictionary = new Dictionary<string, string>();
+
+        static void Main(string[] arguments)
+        {
+            AddPhoneNumber("Police", "911");
+
+            Console.WriteLine(FindPhoneNumber("Police"));
+            DeletePhoneNumber("Police");
+            Console.WriteLine(FindPhoneNumber("Police"));
+
+        }
+
+        public static void AddPhoneNumber(string name, string number)
+        {
+            dictionary.Add(name, number);
+        }
+
+        public static void DeletePhoneNumber(string name)
+        {
+            dictionary.Remove(name);
+        }
+
+        public static string FindPhoneNumber(string name)
+        {
+            string number = "None";
+            if (dictionary.ContainsKey(name)) dictionary.TryGetValue(name, out number);
+            return number;
+        }
+    }
+}
